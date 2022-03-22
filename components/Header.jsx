19cfcb79072400cc0ea2316e35efbe4e4f2cdcb3
Kazim Kayhan <70243719+kazim-kayhan@users.noreply.github.com>
2022-03-22 06:36:03 +0430

    Add My medium title

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Medium2.0 Clone
+              My Medium
             </span>
           </Link>
         </div>
