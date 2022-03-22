6968674bdab9e5a2a8f8f6e92e8137f776032410
Kazim Kayhan <70243719+kazim-kayhan@users.noreply.github.com>
2022-03-22 06:23:09 +0430

    Change title

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Kazim Kayhan
+              Medium2.0 Clone
             </span>
           </Link>
         </div>
